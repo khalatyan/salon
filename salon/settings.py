3895a29e1e6a35ea2f14ebbef6a8fcbd45838f3d
khalatyan <khalatyaaan@gmail.com>
2020-08-02 18:17:52 +0800

    новое бд

--- salon/settings.py
+++ salon/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
